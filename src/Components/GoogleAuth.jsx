f9f5dd7a93e0488c06c30e10091af811650d0457
raju-ahammad <web.rajuahammad@gmail.com>
2020-08-08 23:21:49 +0600

    add google buitton

--- src/Components/GoogleAuth.jsx
+++ src/Components/GoogleAuth.jsx
@@ -20,14 +20,27 @@ export class GoogleAuth extends Component {
     onAuthChange = () => {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() })
     }
+
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
-            return <div>I dont know if we are signed in</div>
+            return null
         } else if (this.state.isSignedIn) {
-            return <div>I am signed in!</div>
+            return (
+                <button className="ui red google button">
+                    <i className="google icon">
+                        Sign Out
+                    </i>
+                </button>
+            );
         }
         else {
-            return <div> I am not signed In</div>
+            return (
+                <button className="ui red google button">
+                    <i className="google icon">
+                        SignIn with google
+                    </i>
+                </button>
+            );
         }
     }
     render() {
