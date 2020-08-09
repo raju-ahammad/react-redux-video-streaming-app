9c478496d148a499bf19fc52922ca00eb1adaedf
raju-ahammad <web.rajuahammad@gmail.com>
2020-08-10 02:40:34 +0600

    create auth reducers

--- src/Components/GoogleAuth.jsx
+++ src/Components/GoogleAuth.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { signIn, signOut } from "../actions"
+
 
 export class GoogleAuth extends Component {
-    state = { isSignedIn: null }
 
     componentDidMount() {
         window.gapi.load("client:auth2", () => {
@@ -10,15 +12,21 @@ export class GoogleAuth extends Component {
                 scope : "email"
             }).then(()=> {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({isSignedIn: this.auth.isSignedIn.get() });
+                
+                this.onAuthChange(this.auth.isSignedIn.get())
                 this.auth.isSignedIn.listen(this.onAuthChange);
 
             });
         });
     };
     
-    onAuthChange = () => {
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    onAuthChange = isSignedIn => {
+        if (isSignedIn) {
+            this.props.signIn();
+        }
+        else {
+            this.props.signOut();
+        }
     }
 
     onSignInClick = () => {
@@ -28,16 +36,16 @@ export class GoogleAuth extends Component {
         this.auth.signOut()
     }
     renderAuthButton() {
-        if (this.state.isSignedIn === null) {
+        if (this.props.isSignedIn === null) {
             return null
-        } else if (this.state.isSignedIn) {
+        } else if (this.props.isSignedIn) {
             return (
                 <button onClick = {this.onSignOutClick}
                     className="ui red google button">
                     <i className="google icon">
                         Sign Out
                     </i>
-                </button>
+                </button>  
             );
         }
         else {
@@ -60,4 +68,8 @@ export class GoogleAuth extends Component {
     }
 }
 
-export default GoogleAuth;
+const mapStateToProps = state => {
+    return { isSignedIn: state.auth.isSignedIn };
+}
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
-
+import AuthReducers from './AuthReducers'
 
 export default combineReducers({
-    replaceMe: () => "Hello"
+    auth: AuthReducers
 })
